4c71c38737be65e4cc78485660d924b765f9fe4e
Ninjia77 <144060169+Ninjia77@users.noreply.github.com>
2023-12-02 12:57:10 +0800

    Update start_node.cpp

--- src/start_node.cpp
+++ src/start_node.cpp
@@ -102,26 +102,3 @@ int main(int argc, char **argv)
     }
     return 0;
 }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    }
-    return 0;
-}
